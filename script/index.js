ab11b5e444b4818b8a8dc27310533a6e3c487748
anjessen <antoinejessen19@gmail.com>
2020-11-05 12:05:09 +0100

    nhhj

--- script/index.js
+++ script/index.js
@@ -2,18 +2,18 @@ const API_KEY = '143998c72c162901544b826be11c98ad';
 
 const url = 'https://api.themoviedb.org/3/movie/550?api_key=143998c72c162901544b826be11c98ad';
 
+console.log('hello');
+const promesse = fetch(url);
 
-async function getMovies() {
-    const resp = await fetch(url);
-    const respData = await resp.json();
-
-    console.log(respData);
-
-    return respData;
-}
-
-getMovies();
-
-
+promesse 
+    .then(async response => {
+        try {
+            const cors = await response.json();
+            console.log(cors);
+        } catch (e){
+            console.log(e);
+        }
+    })
+    .catch(err => console.log(err));
 
 
