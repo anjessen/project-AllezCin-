15d9af2866b9a842cb1da0d222d78beb15624a4b
anjessen <antoinejessen19@gmail.com>
2020-11-06 13:59:52 +0100

    bhhv

--- script/index.js
+++ script/index.js
@@ -1,50 +1,103 @@
 const API_KEY = '143998c72c162901544b826be11c98ad';
 
 const urlTrendMovie = 'https://api.themoviedb.org/3/trending/movie/week?api_key=143998c72c162901544b826be11c98ad';
-const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
-const promesse = fetch(urlTrendMovie);
+const img_url = 'https://image.tmdb.org/t/p/w500/';
 
 let movies;
+let data;
 
-
-function getTrendMovies(){
-
+/*function getTrendMovies(){
+    const promesse = fetch(urlTrendMovie);
     promesse 
         .then(async response => {
             try {
-                const cors = await response.json();
-                console.log(cors);
-            } catch (e){
+                data = await response.json();
+                let counter = 0;
+                while (counter <= 6) {
+                    document.getElementById('trend-movies').innerHTML += `
+                    <div class="trend-movies-box">
+                    <div class="trend-movies-card">
+                    <img src= <img src=${img_url + data.results[counter].poster_path}data-movie-id=${data.results[counter].id}/>
+                    <div class="card-body">
+                    <h3 class="card-title">Venom</h3>
+                    <p class="card-text">2016</p>
+                    <p class="card-text">genre</p>
+                </div>
+            </div>
+        </div>`;
+            counter++;
+            }
+            }
+             catch (e){
                 console.log(e);
             }
         })
         .catch(err => console.log(err));
-}
-getTrendMovies();
+}*/
 
 function moviePoster(movies) {
+
     return movies.map((movie) => {
         return `
-            <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
+            <img src=${IMAGE_URL + data.results.poster_path} data-movie-id=${data.results.id}/>
         `;
     })
 }
 
-function displayTrendCards(){
-let counter = 0;
-   while (counter <= 6) {
-        document.getElementById('trend-movies').innerHTML += `
-        <div class="trend-movies-box">
-        <div class="trend-movies-card">
-            <img class="card-img-top" src="img/Venom.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Venom</h5>
-                <p class="card-text">2016</p>
-                <p class="card-text">genre</p>
+/*function displayTrendCards(trendMovie){
+    let counter = 0;
+        while (counter <= 6) {
+            document.getElementById('trend-movies').innerHTML += `
+            <div class="trend-movies-box">
+            <div class="trend-movies-card">
+            <img src=<img src=${IMAGE_URL + data.results.poster_path}data-movie-id=${data.results.id}/>
+                <div class="card-body">
+                    <h5 class="card-title">Venom</h5>
+                    <p class="card-text">2016</p>
+                    <p class="card-text">genre</p>
+                </div>
             </div>
-        </div>
-    </div>`;
-        counter++;
+        </div>`;
+            counter++;
+        }
     }
-}
-displayTrendCards();
+displayTrendCards();*/
+
+
+const getTrendMovies = () => {
+    return new Promise((res, rej) => {
+        return fetch(urlTrendMovie)
+            .then((response) => response.json())
+            .then((data) => res(data))
+            .catch((e) => {
+                console.log(e);
+                rej(e);
+            });
+    });
+};
+
+const displayTrendCard = async () => {
+    try {
+        const data = await getTrendMovies();
+        console.log(data);
+        let counter = 0;
+                while (counter <= 6) {
+                    document.getElementById('trend-movies').innerHTML += `
+                    <div class="trend-movies-box">
+                    <div class="trend-movies-card">
+                    <img src= <img src="${img_url + data.results[counter].poster_path}">
+                    <div class="card-body">
+                    <h3 class="card-title">Venom</h3>
+                    <p class="card-text">2016</p>
+                    <p class="card-text">genre</p>
+                </div>
+            </div>
+        </div>`;
+            counter++;
+            }
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+displayTrendCard();
