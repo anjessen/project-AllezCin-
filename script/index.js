ed020bca5d13a9906fcf53cdf008019ac91b65b1
anjessen <antoinejessen19@gmail.com>
2020-11-09 11:39:09 +0100

    hnu

--- script/index.js
+++ script/index.js
@@ -4,8 +4,13 @@ const urlTrendMovie = 'https://api.themoviedb.org/3/trending/movie/week?api_key=
 const urlGender = 'https://api.themoviedb.org/3/genre/movie/list?api_key=143998c72c162901544b826be11c98ad&language=en-US';
 //const urlFeatured = 'https://api.themoviedb.org/3/movie/${MovieID}/videos?api_key=112ca2b68890cedc6cd02c2b81593072&language=en-US';
 const img_url ='http://image.tmdb.org/t/p/w300/';
+const urlPopularMovie = 'https://api.themoviedb.org/3/movie/popular?api_key=143998c72c162901544b826be11c98ad&language=en-US'; // Featured
+const urlPopularMovie2 = 'https://api.themoviedb.org/3/movie/popular?api_key=143998c72c162901544b826be11c98ad&language=en-US&page=2'; //more Featured
+const urlPopularMovie3 = 'https://api.themoviedb.org/3/movie/popular?api_key=143998c72c162901544b826be11c98ad&language=en-US&page=3';
+
 
 let data;
+let dataFeatured;
 
 function arrGenre(genderId, gender){
     let genreArr = []
@@ -21,11 +26,34 @@ function arrGenre(genderId, gender){
     return genreArr
 };
 
+const getPopularMovies = () => {
+    return new Promise((res, rej) => {
+        return fetch(urlPopularMovie, urlPopularMovie2, urlPopularMovie3)
+            .then((data) => data.json())
+            .then((data) => res(data))
+            .catch((e) => {
+                console.log(e);
+                rej(e);
+            });
+    });
+};
 
 const getTrendMovies = () => {
     return new Promise((res, rej) => {
         return fetch(urlTrendMovie)
-            .then((response) => response.json())
+            .then((data) => data.json())
+            .then((data) => res(data))
+            .catch((e) => {
+                console.log(e);
+                rej(e);
+            });
+    });
+};
+
+const getGenderMovies = () => {
+    return new Promise((res, rej) => {
+        return fetch(urlGender)
+            .then((data) => data.json())
             .then((data) => res(data))
             .catch((e) => {
                 console.log(e);
@@ -43,9 +71,9 @@ const displayTrendCard = async () => {
                 while (counter < 6) {
                     const genderId = data.results[counter].genre_ids;
                     document.querySelector('#trend-movies').innerHTML += `
-                            <div class="trend-movies-box">
-                            <div class="trend-movies-card">
-                            <img class="img-trend-movies" src="${img_url + data.results[counter].poster_path}">
+                            <div class="movies-box">
+                            <div class="movies-card">
+                            <img class="img-movies" src="${img_url + data.results[counter].poster_path}">
                             <div class="card-body">
                             <h3 class="card-title">${data.results[counter].title}</h3>
                             <p class="card-text">${data.results[counter].release_date.slice(0,4)}</p>
@@ -62,46 +90,33 @@ const displayTrendCard = async () => {
 };
 displayTrendCard();
 
-let dataFeatured;
 
-const getGenderMovies = () => {
-    return new Promise((res, rej) => {
-        return fetch(urlGender)
-            .then((response) => response.json())
-            .then((dataFeatured) => res(dataFeatured))
-            .catch((e) => {
-                console.log(e);
-                rej(e);
-            });
-    });
-};
-
-
-/*const displayFeatureCard = async () => {
+const displayFeatureCard = async () => {
     try {
-        const dataFeatured = await getGenderMovies();
-        const gender = await get
+        const gender = await getGenderMovies();
+        const featured = await getPopularMovies();
         console.log(dataFeatured);
         let counter = 0;
                 while (counter <= 11) {
-                    document.getElementById('featured-movies').innerHTML += `
-                    <div class="featured-movies-box">
-                    <div class="featured-movies-card">
-                    <img class="img-movies" src="${img_url + dataFeatured.results[counter].poster_path}">
-                    <div class="card-body">
-                    <h3 class="card-title">${dataFeatured.results[counter].title}</h3>
-                    <p class="card-text">${dataFeatured.results[counter].release_date}</p>
-                    <p class="card-text">${dataFeatured.results[counter].genre_ids}</p>
-                </div>
-            </div>
-        </div>`;
+                    const genderId = data.results[counter].genre_ids;
+                    document.querySelector('featured-movies').innerHTML += `
+                            <div class="movies-box">
+                            <div class="movies-card">
+                            <img class="img-movies" src="${img_url + data.results[counter].poster_path}">
+                            <div class="card-body">
+                            <h3 class="card-title">${data.results[counter].title}</h3>
+                            <p class="card-text">${data.results[counter].release_date.slice(0,4)}</p>
+                            <p class="card-text">${arrGenre(genderId, gender.genres)}</p>
+                            </div>
+                            </div>
+                        </div>`;
             counter++;
             }
     } catch (e) {
         console.log(e);
     }
 };
-displayFeatureCard();*/
+displayFeatureCard();
 
 
 
